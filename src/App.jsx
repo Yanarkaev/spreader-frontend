fcc1cad5ef92d831291d8c98c79dcb55df434559
Yanarkaev <hasssanbek@gmail.com>
2022-11-03 18:01:43 +0300

    написал компонент для входа в аккаунт

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import styles from "./app.module.scss"
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className={styles.app}>
-     
+     <Main />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Auth from './Auth/Auth';
 import styles from "./main.module.scss"
 
 const Main = () => {
     return (
         <div>
-            
+            <Auth />
         </div>
     );
 };
