d3678f036f9dba2f76d90c8630e53a350811fd60
Yanarkaev <hasssanbek@gmail.com>
2022-11-03 16:20:28 +0300

    минимальный фронт

---
+++ src/components/Main/Aside/Aside.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./aside.module.scss"
+
+const Aside = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Aside;
