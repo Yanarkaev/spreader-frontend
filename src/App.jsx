67b1d35f2d31ab3b430f0180a697517d5858a7e1
Laziyni <soasue@mail.ru>
2022-11-03 20:31:43 +0300

    Отчаянно вынужденный коммит, ничего не менял

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import styles from "./app.module.scss"
-import Main from "./components/Main/Main";
-=======
 import styles from "./app.module.scss";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className={styles.app}>
-     <Main />
+      <Main />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Auth from './Auth/Auth';
-import styles from "./main.module.scss"
+import React from "react";
+import Auth from "./Auth/Auth";
+import styles from "./main.module.scss";
 
 const Main = () => {
-    return (
-        <div>
-            <Auth />
-        </div>
-    );
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
 };
 
 export default Main;
