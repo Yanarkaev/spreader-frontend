200626fec42bcc31f168e8f4b0cd6930dbe5f65f
iznor <iznor.hucuev9507@gmail.comm>
2022-11-04 18:40:08 +0300

    доработал Aside осталось svg

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import style from "./aside.module.scss";
-import chat from "../../assets/Aside/chatIcon.svg";
-import contacts from "../../assets/Aside/contactsIcon.svg";
-import doska from "../../assets/Aside/dashboardIcon.svg";
-import email from "../../assets/Aside/emailIcon.svg";
-import settings from "../../assets/Aside/settingsIcon.svg";
-import tasks from "../../assets/Aside/tasksIcon.svg";
-import deals from "../../assets/Aside/dealsIcon.svg";
-
+import chat from "../../assets/Aside/chat1.svg";
+import contacts from "../../assets/Aside/contacts.svg";
+import doska from "../../assets/Aside/dashboard.svg";
+import email from "../../assets/Aside/email.svg";
+import settings from "../../assets/Aside/settings.svg";
+import tasks from "../../assets/Aside/tasks.svg";
+import deals from "../../assets/Aside/deals.svg";
+// import menuLogo from "../../assets/Aside/profile.svg";
 const Aside = () => {
   return (
     <>
       <div className={style.dashboard}>
-        <header> LOGO </header>
+        <header className={style.logo}> LOGO </header>
         <div className={style.mySelf}>
           <div className={style.nameAndEmail}>
             <div className={style.names}>Sierra Ferguson</div>
@@ -21,27 +21,27 @@ const Aside = () => {
         </div>
 
         <main className={style.menu}>
-          <div className={style.doska}>
+          <div className={style.navItem}>
             <img src={doska} className="img" alt="i" />
             <button>Доска</button>
           </div>
-          <div className={style.tasks}>
+          <div className={style.navItem}>
             <img src={tasks} className="img" alt="i" />
             <button>Задания</button>
           </div>
-          <div className={style.email}>
+          <div className={style.navItem}>
             <img src={email} className="img" alt="i" />
             <button>Email</button>
           </div>
-          <div className={style.contacts}>
+          <div className={style.navItem}>
             <img src={contacts} className="img" alt="i" />
             <button>Контакты</button>
           </div>
-          <div className={style.chat}>
+          <div className={style.navItem}>
             <img src={chat} className="img" alt="i" />
             <button>Чат</button>
           </div>
-          <div className={style.deals}>
+          <div className={style.navItem}>
             <img src={deals} className="img" alt="i" />
             <button>Сделки</button>
           </div>
