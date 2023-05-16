ce063126c63f72483f6e553e1ba04941d5e7763c
Yanarkaev <hasssanbek@gmail.com>
2023-05-16 03:42:50 +0300

    добавил axios, baseURL

--- src/shared/services/base.service.js
+++ src/shared/services/base.service.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-const {NODE_ENV} = process.env
+const { NODE_ENV } = process.env;
 
 export const apiInstance = axios.create({
-  baseURL: NODE_ENV === "development" ? "http://localhost:3001/" : "https://spreader-backend.onrender.com/",
+  baseURL:
+    NODE_ENV === "development"
+      ? "http://localhost:3001/"
+      : "https://spreader-backend.onrender.com/",
 });
