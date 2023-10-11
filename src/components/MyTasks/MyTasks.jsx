984c05255c2b9ab061563a3f49e965d8b5c2b6f9
Yanarkaev <hasssanbek@gmail.com>
2023-10-11 23:56:01 +0300

    удалил ненужный импорт

--- src/components/MyTasks/MyTasks.jsx
+++ src/components/MyTasks/MyTasks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import MyTasksHeader from "../../components/MyTasksHeader/MyTasksHeader";
 import MyTasksTable from "../../components/MyTasksTable/MyTasksTable";
 
